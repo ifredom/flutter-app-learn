7aa63d53d3e672bfa4cc00da6d2db9305e2e2876
ifredom <1950735817@qq.com>
2020-03-06 12:56:17 +0800

    api调用二次封装

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\software\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\flutter_projects\first_app\first_flutter_app"
+export "FLUTTER_APPLICATION_PATH=D:\flutter_projects\AAA_workspace\first_flutter_app"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
---
+++ lib/constants/constants.dart
@@ -0,0 +1,18 @@
+class Constants {
+  ///请求地址
+
+  static const String BASE_URL = 'http://test.xxx.com/api';
+
+  static const String GET_BASE_URL = 'http://test.xxx.com/api';
+
+  static const bool DEBUG = true; // 是否开启debug模式
+
+  /// token
+  static const String USER_BASIC_CODE = 'userBasicCode';
+
+  /// appId
+  static const String APP_ID = 'xxx_appid';
+
+  /// appSecrect
+  static const String APP_SECRET = 'xxx_appsecret';
+}
